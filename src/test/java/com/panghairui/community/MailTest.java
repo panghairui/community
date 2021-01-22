7124decdb71f2049396c9e726a82cdf1ab5771f8
panghairui <3206837094@qq.com>
2021-01-22 15:08:12 +0800

    register finish

---
+++ src/test/java/com/panghairui/community/MailTest.java
@@ -0,0 +1,26 @@
+package com.panghairui.community;
+
+import com.panghairui.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.MessagingException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTest {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("3206837094@qq.com", "Test", "Welcome");
+    }
+
+}
