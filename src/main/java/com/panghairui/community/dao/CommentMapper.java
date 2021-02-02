3ada660f749c805a536e56ae697e82265baf3749
panghairui <3206837094@qq.com>
2021-02-02 11:04:43 +0800

    comment finish

---
+++ src/main/java/com/panghairui/community/dao/CommentMapper.java
@@ -0,0 +1,15 @@
+package com.panghairui.community.dao;
+
+import com.panghairui.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+    List<Comment> selectCommentByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType, int entityId);
+
+}
