e73d08008a63625cbb16b7f46f69da3190f42eb5
panghairui <3206837094@qq.com>
2021-02-03 16:42:56 +0800

    letter finish

--- src/test/java/com/panghairui/community/MapperTests.java
+++ src/test/java/com/panghairui/community/MapperTests.java
@@ -2,9 +2,11 @@ package com.panghairui.community;
 
 import com.panghairui.community.dao.DiscussPostMapper;
 import com.panghairui.community.dao.LoginTicketMapper;
+import com.panghairui.community.dao.MessageMapper;
 import com.panghairui.community.dao.UserMapper;
 import com.panghairui.community.entity.DiscussPost;
 import com.panghairui.community.entity.LoginTicket;
+import com.panghairui.community.entity.Message;
 import com.panghairui.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,6 +31,9 @@ public class MapperTests {
 
     @Autowired(required = false)
     private LoginTicketMapper loginTicketMapper;
+
+    @Autowired(required = false)
+    private MessageMapper messageMapper;
 
     @Test
     public void testSelectUser() {
@@ -101,6 +106,28 @@ public class MapperTests {
         loginTicketMapper.updateStatus("abc", 1);
         loginTicket = loginTicketMapper.selectByTicket("abc");
         System.out.println(loginTicket);
+    }
+
+    @Test
+    public void testSelectLetters() {
+        List<Message> list = messageMapper.selectConversations(111, 0, 20);
+        for(Message message : list) {
+            System.out.println(message);
+        }
+
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112", 0, 10);
+        for(Message message : list) {
+            System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+        count = messageMapper.selectLetterUnreadCount(131, "111_131");
+        System.out.println(count);
     }
 
 
