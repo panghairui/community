e2dd969d2c10076d2cce13615d4879c8d0c55e1a
panghairui <3206837094@qq.com>
2021-02-09 16:32:34 +0800

    Like finish

--- src/main/java/com/panghairui/community/controller/UserController.java
+++ src/main/java/com/panghairui/community/controller/UserController.java
@@ -2,6 +2,7 @@ package com.panghairui.community.controller;
 
 import com.panghairui.community.annotation.LoginRequired;
 import com.panghairui.community.entity.User;
+import com.panghairui.community.service.LikeService;
 import com.panghairui.community.service.UserService;
 import com.panghairui.community.util.CommunityUtil;
 import com.panghairui.community.util.HostHolder;
@@ -44,6 +45,9 @@ public class UserController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private LikeService likeService;
 
     @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
@@ -132,6 +136,23 @@ public class UserController {
         userService.updatePassword(prePassword, newPassword);
         return "redirect:/index";
 
+    }
+
+    // 个人主页
+    @RequestMapping(path = "/profile/{userId}", method = RequestMethod.GET)
+    public String getProfilePage(@PathVariable("userId") int userId, Model model) {
+        User user = userService.findUserById(userId);
+        if(user == null) {
+            throw new RuntimeException("该用户不存在！");
+        }
+
+        // 用户
+        model.addAttribute("user", user);
+        // 点赞数量
+        int likeCount = likeService.findUserLikeCount(userId);
+        model.addAttribute("likeCount", likeCount);
+
+        return "/site/profile";
     }
 
 }
